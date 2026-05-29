5e15d7425e5c9dd1ae96abdcd216f1cc71bb111a
Maksym Bondar <maksym@b0ndar.dev>
2026-05-30 00:11:57 +0200

    Add WPF app shell

---
+++ Algorithms-Simulation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Algorithms_Simulation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
